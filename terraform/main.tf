7c86b619688846243787e7938cd9ad7d336423d1
Ricky Hariady <ricky@sense-os.nl>
2024-08-19 15:41:44 +0700

    add vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,21 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {}
+
+locals {
+  name     = "es"
+  vpc_cidr = "192.168.0.0/16"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs            = local.azs
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+}
