2c0aa6c43c063ffd197efc5cad3086415de1052b
Ricky Hariady <ricky@sense-os.nl>
2024-08-19 15:47:54 +0700

    add ec2 instance module

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,43 @@ module "vpc" {
   azs            = local.azs
   public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
 }
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name           = "es"
+  create_private_key = true
+}
+
+data "aws_ami" "debian" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "debian-12-amd64-*",
+    ]
+  }
+}
+
+module "ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.1"
+
+  name                        = "es-node"
+  ami                         = data.aws_ami.debian.id
+  instance_type               = "t3.micro"
+  availability_zone           = element(module.vpc.azs, 0)
+  subnet_id                   = element(module.vpc.public_subnets, 0)
+
+  key_name                    = module.key_pair.key_pair_name
+
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 10
+    },
+  ]
+}
